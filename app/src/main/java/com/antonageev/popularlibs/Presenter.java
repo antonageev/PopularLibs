593d10b1299ed5e3b8aabe1bb2ec0d73b8298f1a
Anton Ageev <antforwork@mail.ru>
2020-05-14 14:34:00 +0300

    Added: PresenterManager with saving state

--- app/src/main/java/com/antonageev/popularlibs/Presenter.java
+++ app/src/main/java/com/antonageev/popularlibs/Presenter.java
@@ -1,38 +1,43 @@
 package com.antonageev.popularlibs;
 
-public class Presenter {
-    private Model model;
-    private MainView view;
+import android.os.Build;
 
-    public Presenter(MainView view) {
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+public class Presenter extends BasePresenter<Model, MainView>{
+
+    public Presenter(){
         model = new Model();
-        this.view = view;
     }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private int calcNewModelValue(int modelElementIndex){
         int currentValue = model.getElementValueAtIndex(modelElementIndex);
         return currentValue + 1;
     }
-    public void buttonClick(int btnIndex){
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void buttonClick(int btnIndex) {
         int newModelValue;
-        switch (btnIndex){
-            case R.id.btnCounter1:
-                newModelValue = calcNewModelValue(0);
-                model.setElementValueAtIndex(0, newModelValue);
-                view.setButtonText(btnIndex, newModelValue);
-                break;
-            case R.id.btnCounter2:
-                newModelValue = calcNewModelValue(1);
-                model.setElementValueAtIndex(1, newModelValue);
-                view.setButtonText(btnIndex, newModelValue);
-                break;
-            case R.id.btnCounter3:
-                newModelValue = calcNewModelValue(2);
-                model.setElementValueAtIndex(2, newModelValue);
-                view.setButtonText(btnIndex, newModelValue);
-                break;
-        }
+        newModelValue = calcNewModelValue(btnIndex);
+        model.setElementValueAtIndex(btnIndex, newModelValue);
+        view().setButtonText(btnIndex, newModelValue);
     }
 
 
+    @Override
+    protected void updateView() {
+        view().showCounters(model.getmMap());
+    }
 
+    @Override
+    public void bindView(@NonNull MainView view) {
+        super.bindView(view);
+        if (model == null) {
+            model = new Model();
+        }
+        updateView();
+    }
 }
---
+++ app/src/main/java/com/antonageev/popularlibs/PresenterManager.java
@@ -0,0 +1,39 @@
+package com.antonageev.popularlibs;
+
+import android.os.Bundle;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PresenterManager {
+    private static final String KEY_PRESENTER_ID = "presenterId";
+    private static PresenterManager instance;
+    private Map<Long, BasePresenter> presenters;
+    private AtomicLong currentId;
+
+    PresenterManager(){
+        currentId = new AtomicLong();
+        presenters = new HashMap<>();
+    }
+
+    public static PresenterManager getInstance() {
+        if (instance == null){
+            instance = new PresenterManager();
+        }
+        return instance;
+    }
+
+    public void savePresenter(BasePresenter<?, ?> presenter, Bundle outstate){
+        long pId = currentId.incrementAndGet();
+        presenters.put(pId, presenter);
+        outstate.putLong(KEY_PRESENTER_ID, pId);
+    }
+
+    public <P extends BasePresenter<?, ?>> P restorePresenter(Bundle savedInstanceState) {
+        long presenterId = savedInstanceState.getLong(KEY_PRESENTER_ID);
+        P presenter = (P) presenters.get(presenterId);
+        presenters.remove(presenterId);
+        return presenter;
+    }
+}
